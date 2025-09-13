6ad3c2313cfe5d7045c8057617fbc5d113ea9958
tlow22 <thaddeuslow@outlook.com>
2025-09-13 15:41:21 -0600

    create a `ProductsController` via the `generate` CLI

--- config/routes.rb
+++ config/routes.rb
@@ -12,3 +12,18 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+Rails.application.routes.draw do
+  get "/products", to: "products#index"
+
+  get "/products/new", to: "products#new"
+  post "/products", to: "products#create"
+
+  get "/products/:id", to: "products#show"
+
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
+
+  delete "/products/:id", to: "products#destroy"
+end
