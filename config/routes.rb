f3a56eecf1ffd1d13c969689241ad205ef3c8c04
tlow22 <thaddeuslow@outlook.com>
2025-09-14 11:41:41 -0600

    checkin progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get "greetings/hello"
+  
+  # Products routes
+  get "/products", to: "products#index"
+  get "/products/new", to: "products#new", as: :new_product
+  post "/products", to: "products#create"
+  get "/products/:id", to: "products#show", as: :product
+  get "/products/:id/edit", to: "products#edit", as: :edit_product
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
+  delete "/products/:id", to: "products#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,19 +23,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
-
-Rails.application.routes.draw do
-  get "/products", to: "products#index"
-
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
-
-  get "/products/:id", to: "products#show"
-
-  get "/products/:id/edit", to: "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
-
-  delete "/products/:id", to: "products#destroy"
 end
